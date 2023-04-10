b97d81730fea9d5c8985895075c3afce46157e6c
Jeremy Garcia <jeremygarcia9400@gmail.com>
2023-04-09 23:14:45 -0400

    complete lab

--- index.js
+++ index.js
@@ -12,5 +12,22 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  const output = tutorials.map(title => {
+    const arrayOfWords = title.split(' ');
+    const newTitles = arrayOfWords.map(word => {
+      const arrayOfLetters = word.split('');
+      arrayOfLetters[0] = arrayOfLetters[0].toUpperCase();
+      let newWord = '';
+      for (const letter of arrayOfLetters) {
+        newWord = newWord + letter;
+      }
+      return newWord;
+    })
+    let newTitle = '';
+    for (const word of newTitles) {
+      newTitle = newTitle + ' ' + word;
+    }
+    return newTitle.slice(1);
+  })
+  return output;
 }
